9435c77e579ac0b39835b1ad848b4b4e5583f6dd
José Henrique Dias Ferreira <66387724+ShadowsCrow@users.noreply.github.com>
2024-10-02 23:42:44 -0300

    Adicionando mais uma validação

--- src/main/java/com/person/person_service/Main.java
+++ src/main/java/com/person/person_service/Main.java
@@ -2,8 +2,10 @@ package com.person.person_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.person.person_service"})
 public class Main {
 
 	public static void main(String[] args) {
